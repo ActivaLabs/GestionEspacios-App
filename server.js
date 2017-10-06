19799cf0f332a8011d97deea1513ffc262ef8009
ActivaLabs <activalabs@ctactiva.es>
2017-10-06 12:03:45 +0200

    Update

--- server.js
+++ server.js
@@ -7,15 +7,20 @@ app.use(express.static(__dirname + '/public'));
 
 app.get("/sayHello", function (request, response) {
   var user_name = request.query.user_name;
+  guardarName(user_name);
   response.end("Hello " + user_name + "!");
 });
 
 app.listen(port);
-console.log("Listening on port ", port);
 
-require("cf-deployment-tracker-client").track();
+var names[];
 
-setInterval(function() {
-    var hora = new Date();
-    console.log("Hello World! " + hora.getHours() + ":" + hora.getMinutes());
-}, 60000);
+function guardarName(name) {
+    console.log("Hola " + name);
+    if (names.indexOf(name) === -1) {
+        names.push(name);
+        console.log("Un nuevo amigo!");
+    } else {
+        console.log("Un viejo conocido!");
+    }
+}
